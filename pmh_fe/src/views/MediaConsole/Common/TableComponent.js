f2d53ff60851ff8bbdd532be2be192eef3e147d2
Luis Aguilar Vargas <deacr12@gmail.com>
2024-11-08 20:13:38 +0000

    Update TableComponent 01

--- pmh_fe/src/views/MediaConsole/Common/TableComponent.js
+++ pmh_fe/src/views/MediaConsole/Common/TableComponent.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import * as ReactTable from '@tanstack/react-table';
+import _ from 'underscore';
+import { useTranslation } from 'react-i18next';
 import { rankItem } from '@tanstack/match-sorter-utils';
 import styles from '_theme/modules/shared/TableComponent.module.css';
 import TableHeaderComponent from './TableHeaderComponent';
@@ -10,6 +12,9 @@ import { DAILY_REVIEW_TABS } from '_helpers/Utils/mediaConsoleUtil';
 const TableComponent = ({
   tableColumnHeaders,
   data,
+  updateCheckedTableData,
+  fetchHeaderData,
+  isRowAvailable,
   handleRemoveClick,
   onEditBudgetGroup,
   tabName,
@@ -26,15 +31,8 @@ const TableComponent = ({
   );
 
   const fuzzyFilter = (row, columnId, value, addMeta) => {
-    // Rank the item
     const itemRank = rankItem(row.getValue(columnId), value);
-
-    // Store the itemRank info
-    addMeta({ 
-        itemRank 
-    });
-
-    // Return if the item should be filtered in/out
+    addMeta({ itemRank });
     return itemRank.passed;
   };
 
@@ -45,14 +43,8 @@ const TableComponent = ({
     columnResizeMode: 'onChange',
     enableGlobalFilter: true,
     globalFilterFn: fuzzyFilter,
-    filterFns: {
-        fuzzy: fuzzyFilter 
-    },
-    state: {
-         // columnFilters, 
-        globalFilter, 
-        grouping 
-    },
+    filterFns: { fuzzy: fuzzyFilter },
+    state: { globalFilter, grouping },
     onGlobalFilterChange: setGlobalFilter,
     onGroupingChange: setGrouping,
     getCoreRowModel: ReactTable.getCoreRowModel(),
@@ -67,6 +59,29 @@ const TableComponent = ({
 
   const preGlobalFilteredRows = table?.getPreFilteredRowModel()?.rows;
   const headerGroups = table?.getHeaderGroups();
+  selectedDailyReviewMenu.pageId === DAILY_REVIEW_TABS.CAMPAIGN_ADV.id &&
+    headerGroups &&
+    fetchHeaderData?.(headerGroups);
+
+  const handleClick = (e, id) => {
+    const updatedRows = [...data];
+    updatedRows[id] = {
+      ...updatedRows[id],
+      isSelected: e?.target?.checked,
+    };
+    updateCheckedTableData?.(updatedRows);
+  };
+
+  const handleSelectAllClick = (event) => {
+    const updatedRows = [...data];
+    updatedRows.forEach((row) => {
+      row.isSelected = event?.target?.checked;
+    });
+    updateCheckedTableData?.(updatedRows);
+  };
+
+  const rowData = table?.getRowModel().rows;
+  isRowAvailable?.(rowData);
 
   useEffect(() => {
     if (onHeaderGroupsChange) {
@@ -76,7 +91,7 @@ const TableComponent = ({
 
   return (
     <>
-      {table.getRowModel().rows.length ? (
+      {rowData?.length ? (
         <div
           className={`col col-sm-12 ${
             styles['budget-table-container-new-ui']
@@ -91,12 +106,14 @@ const TableComponent = ({
               headerGroups={headerGroups}
               table={table}
               tableData={data}
+              handleSelectAllClick={handleSelectAllClick}
               tabName={tabName}
             />
             <TableBodyComponent
               table={table}
               handleRemoveClick={handleRemoveClick}
               onEditBudgetGroup={onEditBudgetGroup}
+              handleClick={handleClick}
               tabName={tabName}
               handleGroupSubscription={handleGroupSubscription}
               setIsLoading={setIsLoading}
